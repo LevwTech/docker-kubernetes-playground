04ea00b253a69d2ca71f86db8134c8fa43a32494
levvw <abdelraahmanmostafa@gmail.com>
2022-06-07 10:51:51 +0200

    connected and started the node and redis containers

--- visits/index.js
+++ visits/index.js
@@ -4,10 +4,11 @@ const redis = require("redis");
 const app = new express();
 const client = redis.createClient({
   host: "redis-server",
+  port: 6379,
 });
 client.set("visits", 0);
 
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
   client.get("visits", (err, visits) => {
     res.send(`number of visits is ${visits}`);
     client.set("visits", parseInt(visits) + 1);
