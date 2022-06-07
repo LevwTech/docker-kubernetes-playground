6f92f64c2040fa2bde245e8946c9582edbd9c411
levvw <abdelraahmanmostafa@gmail.com>
2022-06-07 09:56:49 +0200

    visits node code

---
+++ visits/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = new express();
+const client = redis.createClient();
+client.set("visits", 0);
+
+app.get("./", (req, res) => {
+  client.get("visits", (err, visits) => {
+    res.send(`number of visits is ${visits}`);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log("listening on port 8081");
+});
