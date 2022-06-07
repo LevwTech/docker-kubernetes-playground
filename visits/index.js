5c43a46d1b444d7f03bc04eda0efa4dfc031c6ff
levvw <abdelraahmanmostafa@gmail.com>
2022-06-07 10:38:52 +0200

    connected the two containers through docker compose and redis client

--- visits/index.js
+++ visits/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const redis = require("redis");
 
 const app = new express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: "redis-server",
+});
 client.set("visits", 0);
 
 app.get("./", (req, res) => {
